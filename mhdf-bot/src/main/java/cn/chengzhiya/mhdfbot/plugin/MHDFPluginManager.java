8089f27abb0ae56489d9fe2b9c53cf3950f68f40
橙汁喵 <292200693@qq.com>
2025-12-16 14:17:38 +0800

    移除debug

--- mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/plugin/MHDFPluginManager.java
+++ mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/plugin/MHDFPluginManager.java
@@ -121,7 +121,7 @@ public final class MHDFPluginManager implements PluginManager {
 
             Field field = plugin.getClass().getSuperclass().getDeclaredField("logger");
             field.setAccessible(true);
-            field.set(plugin, LoggerManager.getInstance().getLogger(pluginInfo.name()));移除调试日志
+            field.set(plugin, LoggerManager.getInstance().getLogger(pluginInfo.name()));
 
             plugin.onEnable();
             pluginInfo.pluginStatus(PluginStatus.ENABLE_DONE);
